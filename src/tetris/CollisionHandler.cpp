08998016316cfdd42c677faa3448c26a78854b3f
alexpearson340 <alexpearson340@gmail.com>
2025-04-30 00:58:54 +1000

    Fixed the bug where blocks clipped into eachother vertically. To get to the bottom of it, also simplified the inconsistent usage of the coordinate variables, and gave them more obvious names.
    We still seem to have a horizontal clipping bug

--- src/tetris/CollisionHandler.cpp
+++ src/tetris/CollisionHandler.cpp
@@ -36,7 +36,7 @@ bool CollisionHandler::handle(Grid& tetronimo, Grid& gameBoard, uint32_t current
 }
 
 void CollisionHandler::handleHorizontal(Grid& tetronimo, Grid& gameBoard)
-{
+{   
     // Move the block horizontally
     tetronimo.move(1, 0);
     if (!checkCollisions(tetronimo, gameBoard))
@@ -81,16 +81,15 @@ void CollisionHandler::freezeTetronimo(Grid& tetronimo, Grid& gameBoard)
     int rowOnGameBoard = tetronimo.getPosY() / BLOCK_SIZE;
     int colOnGameBoard = tetronimo.getPosX() / BLOCK_SIZE;
 
-    for (int i = 0; i < tetronimo.getHeight(); ++i)
+    for (int xIndex = 0; xIndex < tetronimo.getWidth(); ++xIndex)
     {
-        for (int j = 0; j < tetronimo.getWidth(); ++j)
+        for (int yIndex = 0; yIndex < tetronimo.getHeight(); ++yIndex)
         {
-            Block& block = tetronimo.getBlock(i, j);
+            Block& block = tetronimo.getBlock(xIndex, yIndex);
             if (block.exists())
-            {
-                gameBoard.createBlock(
-                    rowOnGameBoard + i, colOnGameBoard + j, block.getTexture());
-                if ((rowOnGameBoard + i) * BLOCK_SIZE < START_LINE)
+            {   
+                gameBoard.createBlock(colOnGameBoard + xIndex, rowOnGameBoard + yIndex, block.getTexture());
+                if ((rowOnGameBoard + yIndex) * BLOCK_SIZE < START_LINE)
                 {
                     mKeepPlaying = false;
                 }
@@ -105,9 +104,9 @@ void CollisionHandler::handleCompletedRows(Grid& tetronimo, Grid& gameBoard)
     int rowOnGameBoard = tetronimo.getPosY() / BLOCK_SIZE;
     int colOnGameBoard = tetronimo.getPosX() / BLOCK_SIZE;
     std::vector<int> completedRows;
-    for (int i = 0; i < tetronimo.getHeight(); ++i)
+    for (int yIndex = 0; yIndex < tetronimo.getHeight(); ++yIndex)
     {
-        int rowNum = rowOnGameBoard + i;
+        int rowNum = rowOnGameBoard + yIndex;
         if ((rowNum < gameBoard.getHeight()) && checkForCompletedRow(rowNum, gameBoard))
         {
             mCompletedRows.push_back(rowNum);
@@ -124,9 +123,9 @@ void CollisionHandler::handleCompletedRows(Grid& tetronimo, Grid& gameBoard)
 
 bool CollisionHandler::checkForCompletedRow(int rowNum, Grid& gameBoard)
 {
-    for (int j = 0; j < gameBoard.getWidth(); ++j)
-    {
-        if (!gameBoard.getBlock(rowNum, j).exists())
+    for (int xIndex = 0; xIndex < gameBoard.getWidth(); ++xIndex)
+    {   
+        if (!gameBoard.getBlock(xIndex, rowNum).exists())
         {
             return false;
         }
@@ -160,9 +159,9 @@ void CollisionHandler::setFlashingTexture(std::vector<size_t> completedRows,
 {
     for (auto rowNum : completedRows)
     {
-        for (auto j = 0; j < gameBoard.getWidth(); ++j)
-        {
-            gameBoard.getBlock(rowNum, j).setTexture(texture);
+        for (auto xIndex = 0; xIndex < gameBoard.getWidth(); ++xIndex)
+        {   
+            gameBoard.getBlock(xIndex, rowNum).setTexture(texture);
         }
     }
     mNumberOfFlashesRemaining -= 1;
@@ -172,33 +171,28 @@ void CollisionHandler::setFlashingTexture(std::vector<size_t> completedRows,
 bool CollisionHandler::checkCollisions(Grid& tetronimo, Grid& gameBoard)
 {
     // find nearest whole number of blocks on game board
-    int rowOnGameBoard = tetronimo.getPosY() / BLOCK_SIZE;
-    int colOnGameBoard = tetronimo.getPosX() / BLOCK_SIZE;
+    int rowOnGameBoard = (tetronimo.getPosY() / BLOCK_SIZE) + 1;
+    int colOnGameBoard = (tetronimo.getPosX() / BLOCK_SIZE);
 
-    // bounday condition for the very bottom row
-    rowOnGameBoard += (rowOnGameBoard >= (gameBoard.getHeight() - tetronimo.getHeight())) ? 0 : 1;
-
-    for (int i = 0; i < tetronimo.getWidth(); ++i)
+    for (int xIndex = 0; xIndex < tetronimo.getWidth(); ++xIndex)
     {
-        for (int j = 0; j < tetronimo.getHeight(); ++j)
+        for (int yIndex = 0; yIndex < tetronimo.getHeight(); ++yIndex)
         {
-            Block& block = tetronimo.getBlock(i, j);
+            Block& block = tetronimo.getBlock(xIndex, yIndex);
             if (block.exists())
             {
                 // Check sides of screen
-                if ((block.getPosX() < 0) || (block.getPosX() + BLOCK_SIZE > SCREEN_WIDTH))
+                if ((block.getPosX() < 0) || ((block.getPosX() + BLOCK_SIZE) > SCREEN_WIDTH))
                 {
                     return false;
                 }
                 // Check top and bottom of playable area
-                if ((block.getPosY() < 0) || (block.getPosY() + BLOCK_SIZE > (N_ROWS * BLOCK_SIZE)))
+                if ((block.getPosY() < 0) || (block.getPosY() + BLOCK_SIZE) >= (N_ROWS * BLOCK_SIZE))
                 {
                     return false;
                 }
-                // Check for an already frozen Tetronimo in the same position as the
-                // Block
-                if (gameBoard.getBlock(rowOnGameBoard + i, colOnGameBoard + j)
-                        .exists())
+                // Check for an already frozen Tetronimo in the same position as the Block
+                if (gameBoard.getBlock(colOnGameBoard + xIndex, rowOnGameBoard + yIndex).exists())
                 {
                     return false;
                 }
